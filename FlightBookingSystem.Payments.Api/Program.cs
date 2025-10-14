796a8c14b077e5c5d773cc66b5969f14c1efb394
Evgeniy Babitskiy <e.babitskiy@datareon.ru>
2025-10-14 22:41:24 +0300

    Replace DBMS from mssql to posgresql for payments.

--- FlightBookingSystem.Payments.Api/Program.cs
+++ FlightBookingSystem.Payments.Api/Program.cs
@@ -6,6 +6,7 @@ using FlightBookingSystem.Payments.Core.Repositories;
 using FlightBookingSystem.Payments.Infrastructure.Repositories;
 using MassTransit;
 using Microsoft.Data.SqlClient;
+using Npgsql;
 using System.Data;
 using System.Reflection;
 
@@ -48,7 +49,7 @@ builder.Services.AddMassTransit(config =>
 
 // Add SQL Connection
 builder.Services.AddScoped<IDbConnection>(sp =>
-    new SqlConnection(builder.Configuration.GetConnectionString("DefaultConnection")));
+    new NpgsqlConnection(builder.Configuration.GetConnectionString("PostgresConnection")));
 
 var app = builder.Build();
 
